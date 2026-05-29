46cdfc261921769e63839f5a6aa9112619ef396f
ziiihhl <a17749680605@outlook.com>
2026-05-29 20:33:49 +0800

    made some changes to scraper.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     gallery_url = input_gallery_url()
     session = build_session()
     title,cnt = get_links_and_download(session, gallery_url)
-    console.print(f"成功下载 {cnt} 张图片，保存到: {SAVE_DIR / title}")
+    console.print(f"Downloaded {cnt} images to: {SAVE_DIR / title}")
+    # console.print(f"成功下载 {cnt} 张图片，保存到: {SAVE_DIR / title}")
 if __name__ == "__main__":
     main()
